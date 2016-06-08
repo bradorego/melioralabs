b8e83d6624ab7c28c446db6531fc9fcea2743585
Brad Orego <me@bradorego.com>
2016-06-08 15:09:37 -0500

    fuck you jsonp

--- dev/js/all.js
+++ dev/js/all.js
@@ -73,43 +73,46 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            var processSuccess = function() {
+              // Success message
+              $('#success').html("<div class='alert alert-success'>");
+              $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                  .append("</button>");
+              $('#success > .alert-success')
+                  .append("<strong>Your message has been sent. </strong>");
+              $('#success > .alert-success')
+                  .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+              //clear all fields
+              $('#contactForm').trigger("reset");
+            };
+            $.ajax({
+                url: "https://api.sendgrid.com/api/mail.send.json",
+                type: "GET",
+                data: {
+                  api_user: "melioralabs",
+                  api_key: "TestWord1",
+                  to: "bradley.orego@gmail.com",
+                  from: "borego@u.rochester.edu",
+                  subject: "Contact from MelioraLabs",
+                  html: "Name: " + name + "<br/>Phone:" + phone + "<br/>Email:" + email + "<br/>Message:<br/>" + message
                 },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+                dataType:"jsonp",
+                cache: false,
+                success: processSuccess,
+                error: function(err) {
+                  console.log(err);
+                  if (err.status === 200) { /// jklol actually a success
+                    return processSuccess();
+                  }
+                  // Fail message
+                  $('#success').html("<div class='alert alert-danger'>");
+                  $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                      .append("</button>");
+                  $('#success > .alert-danger').append("<strong>Whoops! Looks like something went wrong. Please try again later!");
+                  $('#success > .alert-danger').append('</div>');
+                  //clear all fields
+                  $('#contactForm').trigger("reset");
                 },
             })
         },
